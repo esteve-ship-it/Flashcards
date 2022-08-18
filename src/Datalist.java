463625de9dd7466d3e8ae679551c95fbb455b3e1
esteve-ship-it <emsteve@mtu.edu>
2022-08-18 03:47:08 -0400

    Adds new characteristic for questions

--- src/Datalist.java
+++ src/Datalist.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 public class Datalist {
     private ArrayList<String> notes;
     private ArrayList<Vocabulary> wordsToKnow;
+    private ArrayList<Question> questionList;
 
     public ArrayList<String> getNotes(){
         return notes;
---
+++ src/Question.java
@@ -0,0 +1,22 @@
+public class Question {
+    private String question;
+    private String answer;
+
+    public Question(String question, String answer){
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public void setQuestion(String question){
+        this.question = question;
+    }
+    public String getQuestion(){
+        return question;
+    }
+    public void setAnswer(String answer){
+        this.answer = answer;
+    }
+    public String getAnswer(){
+        return answer;
+    }
+}
